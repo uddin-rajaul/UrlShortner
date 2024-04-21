871138fcac41dfd0e364650f7b30201bef3067b6
Rajaul <uddin.rajaul1@gmail.com>
2024-04-22 00:59:57 +0545

    Fix vercel json file

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,4 @@
 echo " BUILD SRART"
-python3.11 -m pip install -r requirements.txt
-python3.11 manage.py collectstatic --input --clear
+python3.9 -m pip install -r requirements.txt
+python3.9 manage.py collectstatic --input --clear
 echo " BUILD END"

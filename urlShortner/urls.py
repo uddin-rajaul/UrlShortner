2fbe86a7b7ccf2851460e42ca3a461e72d4347ad
Rajaul <uddin.rajaul1@gmail.com>
2024-04-22 00:10:48 +0545

    remove unique filter for orignal url

--- urlShortner/urls.py
+++ urlShortner/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from urlShortner.views import ShortnerCreateApiView, ShortnerListApiView
+from . import views
 
 urlpatterns = [
-    path('', ShortnerListApiView.as_view(), name='all_list'),
+    path('', views.index, name='index'),
+    path('list', ShortnerListApiView.as_view(), name='all_list'),
     path('create/', ShortnerCreateApiView.as_view(), name='create_'),
 ]
--- urlShortner/views.py
+++ urlShortner/views.py
@@ -1,3 +1,4 @@
+from django.http import JsonResponse
 from django.shortcuts import render, redirect
 from rest_framework.generics import ListAPIView, CreateAPIView
 from .models import Urlshortner
@@ -8,6 +9,8 @@ from django.conf import settings
 from .serializers import LinkSerializer 
 
 # Create your views here.
+
+
 class ShortnerListApiView(ListAPIView):
     queryset = Urlshortner.objects.all()
     serializer_class = LinkSerializer
@@ -23,3 +26,7 @@ class RedirectLink(View):
             return redirect(redirect_link)
         else:
             return redirect('/404/')
+
+
+def index(request):
+    return render(request, 'base.html')
